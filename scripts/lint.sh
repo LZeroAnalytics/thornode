8564fdf9fb860a4da5be82dfd7d9b048e894e652
Ursa (9R) <ursa@ninerealms.co>
2023-09-08 02:15:39 +0000

    [add] Commit Generated Files

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -12,13 +12,10 @@ if ! grep "^  version: ${version}" openapi/openapi.yaml; then
   die "docs version (openapi/openapi.yaml) does not match version file ${version}"
 fi
 
-# Check that no .pb.go files were added.
-if git ls-files '*.go' | grep -q '.pb.go$'; then
-  die "Do not add generated protobuf .pb.go files"
-fi
+FILTER=(-e '^docs/' -e '.pb.go$' -e '^openapi/gen' -e '_gen.go')
 
-if [ -n "$(git ls-files '*.go' | grep -v -e '^docs/' | xargs gofumpt -l 2>/dev/null)" ]; then
-  git ls-files '*.go' | grep -v -e '^docs/' | xargs gofumpt -d 2>/dev/null
+if [ -n "$(git ls-files '*.go' | grep -v "${FILTER[@]}" | xargs gofumpt -l 2>/dev/null)" ]; then
+  git ls-files '*.go' | grep -v "${FILTER[@]}" | xargs gofumpt -d 2>/dev/null
   die "Go formatting errors"
 fi
 go mod verify
