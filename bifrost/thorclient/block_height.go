5dda31594387f597ab388623835339776c53007b
Multipartite <sonaebito@gmail.com>
2023-10-31 01:30:35 +0000

    [Version-unspecific] Cache responses for Bifrost get

--- bifrost/thorclient/block_height.go
+++ bifrost/thorclient/block_height.go
@@ -3,10 +3,8 @@ package thorclient
 import (
 	"encoding/json"
 	"fmt"
-	"time"
 
 	"gitlab.com/thorchain/thornode/common"
-	"gitlab.com/thorchain/thornode/constants"
 	"gitlab.com/thorchain/thornode/x/thorchain/types"
 )
 
@@ -40,16 +38,11 @@ func (b *thorchainBridge) GetLastSignedOutHeight(chain common.Chain) (int64, err
 
 // GetBlockHeight returns the current height for thorchain blocks
 func (b *thorchainBridge) GetBlockHeight() (int64, error) {
-	if time.Since(b.lastBlockHeightCheck) < constants.ThorchainBlockTime && b.lastThorchainBlockHeight > 0 {
-		return b.lastThorchainBlockHeight, nil
-	}
 	latestBlocks, err := b.getLastBlock(common.EmptyChain)
 	if err != nil {
 		return 0, fmt.Errorf("failed to GetThorchainHeight: %w", err)
 	}
-	b.lastBlockHeightCheck = time.Now()
 	for _, item := range latestBlocks {
-		b.lastThorchainBlockHeight = item.Thorchain
 		return item.Thorchain, nil
 	}
 	return 0, fmt.Errorf("failed to GetThorchainHeight")
