2d797664991703deea145536de1bdb8fe5ff7996
Ursa (9R) <ursa@ninerealms.co>
2023-11-06 17:55:48 +0000

    Litecoin UTXO Client V2

--- bifrost/pkg/chainclients/utxo/signer.go
+++ bifrost/pkg/chainclients/utxo/signer.go
@@ -5,12 +5,15 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"reflect"
 	"strings"
 	"sync"
 
 	bchwire "github.com/gcash/bchd/wire"
 	"github.com/gcash/bchutil"
 	"github.com/hashicorp/go-multierror"
+	ltcwire "github.com/ltcsuite/ltcd/wire"
+	"github.com/ltcsuite/ltcutil"
 
 	"github.com/btcsuite/btcd/mempool"
 	"github.com/btcsuite/btcd/wire"
@@ -83,6 +86,12 @@ func (c *Client) SignTx(tx stypes.TxOutItem, thorchainHeight int64) ([]byte, []b
 			return nil, nil, nil, fmt.Errorf("fail to decode next address: %w", err)
 		}
 		outputAddrStr = outputAddr.(bchutil.Address).String() // trunk-ignore(golangci-lint/forcetypeassert)
+	case common.LTCChain:
+		outputAddr, err = ltcutil.DecodeAddress(tx.ToAddress.String(), c.getChainCfgLTC())
+		if err != nil {
+			return nil, nil, nil, fmt.Errorf("fail to decode next address: %w", err)
+		}
+		outputAddrStr = outputAddr.(ltcutil.Address).String() // trunk-ignore(golangci-lint/forcetypeassert)
 	default:
 		c.log.Fatal().Msg("unsupported chain")
 	}
@@ -93,10 +102,11 @@ func (c *Client) SignTx(tx stypes.TxOutItem, thorchainHeight int64) ([]byte, []b
 		return nil, nil, nil, nil
 	}
 	switch outputAddr.(type) {
-	case *dogutil.AddressPubKey, *bchutil.AddressPubKey:
+	case *dogutil.AddressPubKey, *bchutil.AddressPubKey, *ltcutil.AddressPubKey:
 		c.log.Info().Msgf("address: %s is address pubkey type, should not be used", outputAddrStr)
 		return nil, nil, nil, nil
 	default: // keep lint happy
+		fmt.Println("type", reflect.TypeOf(outputAddr))
 	}
 
 	// load from checkpoint if it exists
@@ -146,6 +156,8 @@ func (c *Client) SignTx(tx stypes.TxOutItem, thorchainHeight int64) ([]byte, []b
 		stx = wireToDOGE(redeemTx)
 	case common.BCHChain:
 		stx = wireToBCH(redeemTx)
+	case common.LTCChain:
+		stx = wireToLTC(redeemTx)
 	default:
 		c.log.Fatal().Msg("unsupported chain")
 	}
@@ -163,10 +175,11 @@ func (c *Client) SignTx(tx stypes.TxOutItem, thorchainHeight int64) ([]byte, []b
 			// chain specific signing
 			switch c.cfg.ChainID {
 			case common.DOGEChain:
-				// trunk-ignore(golangci-lint/forcetypeassert)
 				err = c.signUTXODOGE(stx.(*dogewire.MsgTx), tx, amount, sourceScript, i, thorchainHeight)
 			case common.BCHChain:
 				err = c.signUTXOBCH(stx.(*bchwire.MsgTx), tx, amount, sourceScript, i, thorchainHeight)
+			case common.LTCChain:
+				err = c.signUTXOLTC(stx.(*ltcwire.MsgTx), tx, amount, sourceScript, i, thorchainHeight)
 			default:
 				c.log.Fatal().Msg("unsupported chain")
 			}
@@ -190,6 +203,8 @@ func (c *Client) SignTx(tx stypes.TxOutItem, thorchainHeight int64) ([]byte, []b
 		redeemTx = dogeToWire(stx.(*dogewire.MsgTx))
 	case common.BCHChain:
 		redeemTx = bchToWire(stx.(*bchwire.MsgTx))
+	case common.LTCChain:
+		redeemTx = ltcToWire(stx.(*ltcwire.MsgTx))
 	default:
 		c.log.Fatal().Msg("unsupported chain")
 	}
@@ -267,14 +282,16 @@ func (c *Client) BroadcastTx(txOut stypes.TxOutItem, payload []byte) (string, er
 		return "", fmt.Errorf("fail to deserialize payload: %w", err)
 	}
 
-	// broadcast tx
-	var txid string
+	var maxFee any
 	switch c.cfg.ChainID {
 	case common.DOGEChain, common.BCHChain:
-		txid, err = c.rpc.SendRawTransaction(redeemTx, true)
-	default:
-		c.log.Fatal().Msg("unsupported chain")
+		maxFee = true
+	case common.LTCChain:
+		maxFee = 10_000_000
 	}
+
+	// broadcast tx
+	txid, err := c.rpc.SendRawTransaction(redeemTx, maxFee)
 	if txid != "" {
 		bm.AddSelfTransaction(txid)
 	}
