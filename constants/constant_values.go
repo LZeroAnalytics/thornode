bef833948539d8d0ccdfb289decc215bb7800ac8
Ursa <ursa@ninerealms.co>
2023-11-15 10:34:44 -0500

    [cleanup] Use Stringer for Type Strings

--- constants/constant_values.go
+++ constants/constant_values.go
@@ -6,7 +6,9 @@ import (
 	"github.com/blang/semver"
 )
 
-// ConstantName the name we used to get constant values
+// ConstantName the name we used to get constant values.
+//
+//go:generate stringer -type=ConstantName
 type ConstantName int
 
 const (
@@ -133,140 +135,6 @@ const (
 	KeygenRetryInterval
 	SaversStreamingSwapsInterval
 )
-
-var nameToString = map[ConstantName]string{
-	EmissionCurve:                       "EmissionCurve",
-	IncentiveCurve:                      "IncentiveCurve",
-	MaxRuneSupply:                       "MaxRuneSupply",
-	BlocksPerYear:                       "BlocksPerYear",
-	OutboundTransactionFee:              "OutboundTransactionFee",
-	NativeOutboundFeeUSD:                "NativeOutboundFeeUSD",
-	NativeTransactionFee:                "NativeTransactionFee",
-	NativeTransactionFeeUSD:             "NativeTransactionFeeUSD",
-	PoolCycle:                           "PoolCycle",
-	MinRunePoolDepth:                    "MinRunePoolDepth",
-	MaxAvailablePools:                   "MaxAvailablePools",
-	StagedPoolCost:                      "StagedPoolCost",
-	PendingLiquidityAgeLimit:            "PendingLiquidityAgeLimit",
-	KillSwitchStart:                     "KillSwitchStart",          // TODO remove on hard fork
-	KillSwitchDuration:                  "KillSwitchDuration",       // TODO remove on hard fork
-	MinimumNodesForYggdrasil:            "MinimumNodesForYggdrasil", // TODO remove on hard fork
-	MinimumNodesForBFT:                  "MinimumNodesForBFT",
-	DesiredValidatorSet:                 "DesiredValidatorSet",
-	AsgardSize:                          "AsgardSize",
-	DerivedDepthBasisPts:                "DerivedDepthBasisPts",
-	DerivedMinDepth:                     "DerivedMinDepth",
-	MaxAnchorSlip:                       "MaxAnchorSlip",
-	MaxAnchorBlocks:                     "MaxAnchorBlocks",
-	DynamicMaxAnchorSlipBlocks:          "DynamicMaxAnchorSlipBlocks",
-	DynamicMaxAnchorTarget:              "DynamicMaxAnchorTarget",
-	DynamicMaxAnchorCalcInterval:        "DynamicMaxAnchorCalcInterval",
-	ChurnInterval:                       "ChurnInterval",
-	ChurnRetryInterval:                  "ChurnRetryInterval",
-	ValidatorsChangeWindow:              "ValidatorsChangeWindow",
-	LeaveProcessPerBlockHeight:          "LeaveProcessPerBlockHeight",
-	BadValidatorRedline:                 "BadValidatorRedline",
-	LackOfObservationPenalty:            "LackOfObservationPenalty",
-	SigningTransactionPeriod:            "SigningTransactionPeriod",
-	DoubleSignMaxAge:                    "DoubleSignMaxAge",
-	PauseBond:                           "PauseBond",
-	PauseUnbond:                         "PauseUnbond",
-	MinimumBondInRune:                   "MinimumBondInRune",
-	MaxBondProviders:                    "MaxBondProviders",
-	FundMigrationInterval:               "FundMigrationInterval",
-	ArtificialRagnarokBlockHeight:       "ArtificialRagnarokBlockHeight",
-	MaximumLiquidityRune:                "MaximumLiquidityRune",
-	StrictBondLiquidityRatio:            "StrictBondLiquidityRatio",
-	DefaultPoolStatus:                   "DefaultPoolStatus",
-	MaxOutboundAttempts:                 "MaxOutboundAttempts",
-	SlashPenalty:                        "SlashPenalty",
-	PauseOnSlashThreshold:               "PauseOnSlashThreshold",
-	FailKeygenSlashPoints:               "FailKeygenSlashPoints",
-	FailKeysignSlashPoints:              "FailKeysignSlashPoints",
-	LiquidityLockUpBlocks:               "LiquidityLockUpBlocks",
-	ObserveSlashPoints:                  "ObserveSlashPoints",
-	MissBlockSignSlashPoints:            "MissBlockSignSlashPoints",
-	ObservationDelayFlexibility:         "ObservationDelayFlexibility",
-	StopFundYggdrasil:                   "StopFundYggdrasil", // TODO remove on hard fork
-	YggFundLimit:                        "YggFundLimit",      // TODO remove on hard fork
-	YggFundRetry:                        "YggFundRetry",      // TODO remove on hard fork
-	JailTimeKeygen:                      "JailTimeKeygen",
-	JailTimeKeysign:                     "JailTimeKeysign",
-	NodePauseChainBlocks:                "NodePauseChainBlocks",
-	EnableDerivedAssets:                 "EnableDerivedAssets",
-	MinSwapsPerBlock:                    "MinSwapsPerBlock",
-	MaxSwapsPerBlock:                    "MaxSwapsPerBlock",
-	EnableOrderBooks:                    "EnableOrderBooks",
-	MintSynths:                          "MintSynths",
-	BurnSynths:                          "BurnSynths",
-	VirtualMultSynths:                   "VirtualMultSynths",
-	VirtualMultSynthsBasisPoints:        "VirtualMultSynthsBasisPoints",
-	MaxSynthPerAssetDepth:               "MaxSynthPerAssetDepth", // TODO: remove me on hard fork
-	MaxSynthPerPoolDepth:                "MaxSynthPerPoolDepth",
-	MaxSynthsForSaversYield:             "MaxSynthsForSaversYield",
-	MinSlashPointsForBadValidator:       "MinSlashPointsForBadValidator",
-	FullImpLossProtectionBlocks:         "FullImpLossProtectionBlocks",
-	BondLockupPeriod:                    "BondLockupPeriod",
-	NumberOfNewNodesPerChurn:            "NumberOfNewNodesPerChurn",
-	MinTxOutVolumeThreshold:             "MinTxOutVolumeThreshold",
-	TxOutDelayRate:                      "TxOutDelayRate",
-	TxOutDelayMax:                       "TxOutDelayMax",
-	MaxTxOutOffset:                      "MaxTxOutOffset",
-	TNSRegisterFee:                      "TNSRegisterFee",
-	TNSRegisterFeeUSD:                   "TNSRegisterFeeUSD",
-	TNSFeeOnSale:                        "TNSFeeOnSale",
-	TNSFeePerBlock:                      "TNSFeePerBlock",
-	TNSFeePerBlockUSD:                   "TNSFeePerBlockUSD",
-	PermittedSolvencyGap:                "PermittedSolvencyGap",
-	ValidatorMaxRewardRatio:             "ValidatorMaxRewardRatio",
-	NodeOperatorFee:                     "NodeOperatorFee",
-	PoolDepthForYggFundingMin:           "PoolDepthForYggFundingMin", // TODO remove on hard fork
-	MaxNodeToChurnOutForLowVersion:      "MaxNodeToChurnOutForLowVersion",
-	ChurnOutForLowVersionBlocks:         "ChurnOutForLowVersionBlocks",
-	SwapOutDexAggregationDisabled:       "SwapOutDexAggregationDisabled",
-	POLMaxNetworkDeposit:                "POLMaxNetworkDeposit",
-	POLMaxPoolMovement:                  "POLMaxPoolMovement",
-	POLSynthUtilization:                 "POLSynthUtilization", // TODO: remove me on hard fork
-	POLTargetSynthPerPoolDepth:          "POLTargetSynthPerPoolDepth",
-	POLBuffer:                           "POLBuffer",
-	RagnarokProcessNumOfLPPerIteration:  "RagnarokProcessNumOfLPPerIteration",
-	SynthYieldBasisPoints:               "SynthYieldBasisPoints",
-	SynthYieldCycle:                     "SynthYieldCycle",
-	MinimumL1OutboundFeeUSD:             "MinimumL1OutboundFeeUSD",
-	MinimumPoolLiquidityFee:             "MinimumPoolLiquidityFee",
-	ILPCutoff:                           "ILPCutoff",
-	ChurnMigrateRounds:                  "ChurnMigrateRounds",
-	MaxAffiliateFeeBasisPoints:          "MaxAffiliateFeeBasisPoints",
-	StreamingSwapPause:                  "StreamingSwapPause",
-	StreamingSwapMinBPFee:               "StreamingSwapMinBPFee",
-	StreamingSwapMaxLength:              "StreamingSwapMaxLength",
-	StreamingSwapMaxLengthNative:        "StreamingSwapMaxLengthNative",
-	MinCR:                               "MinCR",
-	MaxCR:                               "MaxCR",
-	LoanStreamingSwapsInterval:          "LoanStreamingSwapsInterval",
-	PauseLoans:                          "PauseLoans",
-	LoanRepaymentMaturity:               "LoanRepaymentMaturity",
-	LendingLever:                        "LendingLever",
-	AllowWideBlame:                      "AllowWideBlame",
-	TargetOutboundFeeSurplusRune:        "TargetOutboundFeeSurplusRune",
-	MaxOutboundFeeMultiplierBasisPoints: "MaxOutboundFeeMultiplierBasisPoints",
-	MinOutboundFeeMultiplierBasisPoints: "MinOutboundFeeMultiplierBasisPoints",
-	EnableUSDFees:                       "EnableUSDFees",
-	PreferredAssetOutboundFeeMultiplier: "PreferredAssetOutboundFeeMultiplier",
-	FeeUSDRoundSignificantDigits:        "FeeUSDRoundSignificantDigits",
-	MigrationVaultSecurityBps:           "MigrationVaultSecurityBps",
-	KeygenRetryInterval:                 "KeygenRetryInterval",
-	SaversStreamingSwapsInterval:        "SaversStreamingSwapsInterval",
-}
-
-// String implement fmt.stringer
-func (cn ConstantName) String() string {
-	val, ok := nameToString[cn]
-	if !ok {
-		return "NA"
-	}
-	return val
-}
 
 // ConstantValues define methods used to get constant values
 type ConstantValues interface {
