bfb01d9822b62be9ca894a8598bd08edffa8a10b
Ursa <ursa@ninerealms.co>
2023-11-30 15:40:01 -0500

    [utxo] Improve Re-Org Rescan Handling

--- bifrost/pkg/chainclients/utxo/bitcoincash_test.go
+++ bifrost/pkg/chainclients/utxo/bitcoincash_test.go
@@ -97,6 +97,7 @@ func (s *BitcoinCashSuite) SetUpTest(c *C) {
 	s.cfg.UTXO.TransactionBatchSize = 100
 	s.cfg.UTXO.MaxMempoolBatches = 10
 	s.cfg.UTXO.EstimatedAverageTxSize = 1500
+	s.cfg.UTXO.MaxReorgRescanBlocks = 1
 	ns := strconv.Itoa(time.Now().Nanosecond())
 	ctypes.Network = ctypes.TestNetwork
 
@@ -854,10 +855,18 @@ func (s *BitcoinCashSuite) TestOnObservedTxIn(c *C) {
 
 func (s *BitcoinCashSuite) TestProcessReOrg(c *C) {
 	// can't get previous block meta should not error
-	var result btcjson.GetBlockVerboseTxResult
+	type response struct {
+		Result btcjson.GetBlockVerboseResult `json:"result"`
+	}
+	res := response{}
 	blockContent, err := os.ReadFile("../../../../test/fixtures/bch/block.json")
 	c.Assert(err, IsNil)
-	c.Assert(json.Unmarshal(blockContent, &result), IsNil)
+	c.Assert(json.Unmarshal(blockContent, &res), IsNil)
+	result := btcjson.GetBlockVerboseTxResult{
+		Hash:         res.Result.Hash,
+		PreviousHash: res.Result.PreviousHash,
+		Height:       res.Result.Height,
+	}
 	// should not trigger re-org process
 	reOrgedTxs, err := s.client.processReorg(&result)
 	c.Assert(err, IsNil)
