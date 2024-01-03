31ea90702e13e786994bf21335e96016a900955b
Multipartite <sonaebito@gmail.com>
2024-01-03 16:48:35 +0200

    Squashed Oleg commits

--- common/tokenlist/avaxtokens/avax_mocknet.go
+++ common/tokenlist/avaxtokens/avax_mocknet.go
@@ -10,5 +10,8 @@ import (
 //go:embed avax_mocknet_V95.json
 var AVAXTokenListRawV95 []byte
 
-//go:embed avax_mocknet_latest.json
+//go:embed avax_mocknet_V101.json
 var AVAXTokenListRawV101 []byte
+
+//go:embed avax_mocknet_latest.json
+var AVAXTokenListRawV126 []byte
