793861588153c3a0244ecf375ca7e49d5f8a8698
Multipartite <sonaebito@gmail.com>
2024-01-23 14:02:58 +0000

    [V127-specific] Deduct pending outbounds to get available funds in LackSigning

--- x/thorchain/keeper/keeper_dummy.go
+++ x/thorchain/keeper/keeper_dummy.go
@@ -401,7 +401,10 @@ func (k KVStoreDummy) GetLeastSecure(_ cosmos.Context, _ Vaults, _ int64) Vault 
 func (k KVStoreDummy) GetMostSecure(_ cosmos.Context, _ Vaults, _ int64) Vault       { return Vault{} }
 func (k KVStoreDummy) GetMostSecureStrict(_ cosmos.Context, _ Vaults, _ int64) Vault { return Vault{} }
 func (k KVStoreDummy) SortBySecurity(_ cosmos.Context, _ Vaults, _ int64) Vaults     { return nil }
-func (k KVStoreDummy) DeleteVault(_ cosmos.Context, _ common.PubKey) error           { return kaboom }
+func (k KVStoreDummy) GetPendingOutbounds(_ cosmos.Context, _ common.Asset) []TxOutItem {
+	return nil
+}
+func (k KVStoreDummy) DeleteVault(_ cosmos.Context, _ common.PubKey) error { return kaboom }
 
 func (k KVStoreDummy) HasValidVaultPools(_ cosmos.Context) (bool, error)         { return false, kaboom }
 func (k KVStoreDummy) AddPoolFeeToReserve(_ cosmos.Context, _ cosmos.Uint) error { return kaboom }
