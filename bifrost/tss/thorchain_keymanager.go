25d75ac2b4b5e8e05e39065cb0246ea946349e91
Pluto (9R) <pluto@ninerealms.co>
2024-02-05 15:54:39 +0000

    Strip bnb-chain SDK from non-BNB codebase

--- bifrost/tss/thorchain_keymanager.go
+++ bifrost/tss/thorchain_keymanager.go
@@ -1,16 +1,40 @@
 package tss
 
 import (
-	"gitlab.com/thorchain/binance-sdk/keys"
-	"gitlab.com/thorchain/binance-sdk/types/tx"
-
-	"gitlab.com/thorchain/thornode/common"
+	"github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth/legacy/legacytx"
+	"github.com/tendermint/tendermint/crypto"
 )
+
+type EncryptedKeyJSON struct {
+	Address string     `json:"address"`
+	Crypto  CryptoJSON `json:"crypto"`
+	Id      string     `json:"id"`
+	Version int        `json:"version"`
+}
+type CryptoJSON struct {
+	Cipher       string                 `json:"cipher"`
+	CipherText   string                 `json:"ciphertext"`
+	CipherParams cipherparamsJSON       `json:"cipherparams"`
+	KDF          string                 `json:"kdf"`
+	KDFParams    map[string]interface{} `json:"kdfparams"`
+	MAC          string                 `json:"mac"`
+}
+
+type cipherparamsJSON struct {
+	IV string `json:"iv"`
+}
 
 // ThorchainKeyManager it is a composite of binance chain keymanager
 type ThorchainKeyManager interface {
-	keys.KeyManager
-	SignWithPool(msg tx.StdSignMsg, poolPubKey common.PubKey) ([]byte, error)
+	Sign(legacytx.StdSignMsg) ([]byte, error)
+	GetPrivKey() crypto.PrivKey
+	GetAddr() types.AccAddress
+
+	ExportAsMnemonic() (string, error)
+	ExportAsPrivateKey() (string, error)
+	ExportAsKeyStore(password string) (*EncryptedKeyJSON, error)
+
 	RemoteSign(msg []byte, poolPubKey string) ([]byte, []byte, error)
 	Start()
 	Stop()
