fbc8484e67168ebac7c75ee1d18858f94dbe31d1
Multipartite <sonaebito@gmail.com>
2024-03-01 17:43:22 +0000

    [Version-unspecific] Deprecate Coins Add, Coins SafeSub, and Gas Add #check-lint-warning

--- x/thorchain/manager_gas_v99.go
+++ x/thorchain/manager_gas_v99.go
@@ -46,7 +46,7 @@ func (gm *GasMgrV99) BeginBlock(mgr Manager) {
 
 // AddGasAsset to the EventGas
 func (gm *GasMgrV99) AddGasAsset(gas common.Gas, increaseTxCount bool) {
-	gm.gas = gm.gas.Add(gas)
+	gm.gas = gm.gas.Adds_deprecated(gas)
 	if !increaseTxCount {
 		return
 	}
