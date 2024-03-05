936a99e6d0e91f8918c7f111560c6b4891d65574
Pluto (9R) <pluto@ninerealms.co>
2024-03-05 14:22:09 +0000

    Cleanup unused argument + CI fixes

--- common/random_string.go
+++ common/random_string.go
@@ -1,22 +1,22 @@
 package common
 
-import "math/rand"
+import (
+	"crypto/rand"
+	"math/big"
+)
 
 const hexBytes = "abcdefABCDEF0123456789"
-const (
-	hexIdxBits = 5                 // 5 bits to represent a hex index
-	hexIdxMask = 1<<hexIdxBits - 1 // All 1-bits, as many as hexIdxBits
-)
 
 // RandHexString generates random hex string used for test purpose
 func RandHexString(n int) string {
-	b := make([]byte, n)
-	for i := 0; i < n; {
-		// #nosec G404 this is a method only used for test purpose
-		if idx := int(rand.Int31() & hexIdxMask); idx < len(hexBytes) {
-			b[i] = hexBytes[idx]
-			i++
+	ret := make([]byte, n)
+	for i := 0; i < n; i++ {
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(hexBytes))))
+		if err != nil {
+			return ""
 		}
+		ret[i] = hexBytes[num.Int64()]
 	}
-	return string(b)
+
+	return string(ret)
 }
