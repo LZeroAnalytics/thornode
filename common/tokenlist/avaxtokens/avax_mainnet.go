0d4680ae96dd16df7cc99974726e0c70f480d34e
Eridanus (9R) <eridanus@ninerealms.co>
2024-04-01 19:14:09 +0000

    [V131] Whitelist BSC Tokens for pool creation

--- common/tokenlist/avaxtokens/avax_mainnet.go
+++ common/tokenlist/avaxtokens/avax_mainnet.go
@@ -16,5 +16,8 @@ var AVAXTokenListRawV101 []byte
 //go:embed avax_mainnet_V126.json
 var AVAXTokenListRawV126 []byte
 
-//go:embed avax_mainnet_latest.json
+//go:embed avax_mainnet_V127.json
 var AVAXTokenListRawV127 []byte
+
+//go:embed avax_mainnet_latest.json
+var AVAXTokenListRawV131 []byte
--- common/tokenlist/bsctokens/bsc_mocknet.go
+++ common/tokenlist/bsctokens/bsc_mocknet.go
@@ -8,5 +8,8 @@ import _ "embed"
 //go:embed bsc_mocknet_V111.json
 var BSCTokenListRawV111 []byte
 
-//go:embed bsc_mocknet_latest.json
+//go:embed bsc_mocknet_V122.json
 var BSCTokenListRawV122 []byte
+
+//go:embed bsc_mocknet_latest.json
+var BSCTokenListRawV131 []byte
