84c0151fe365862a07255af25f53a203d7d0f259
Asmund THORSec <9491667-AsmundTHORSec@users.noreply.gitlab.com>
2024-06-04 20:48:23 +0000

    [ci] Fix ERC20 list linter to use latest

--- scripts/lint-erc20s.bash
+++ scripts/lint-erc20s.bash
@@ -1,28 +1,43 @@
 #!/bin/bash
 set -euo pipefail
 
-# Ethereum tokens
-git show origin/develop:common/tokenlist/ethtokens/eth_mainnet_V95.json |
-  jq -r '.tokens[] | .address | ascii_downcase' | sort -n | uniq -u >/tmp/orig_erc20_token_list.txt
+possibly_die() {
+  if [[ ${CI_MERGE_REQUEST_TITLE-} == *"#check-lint-warning"* ]]; then
+    echo "Merge request is marked unsafe. Please carefully review errors above."
+  else
+    exit 1
+  fi
+}
 
-jq -r '.tokens[] | .address | ascii_downcase' <common/tokenlist/ethtokens/eth_mainnet_V95.json |
-  uniq -u >/tmp/modified_erc20_token_list.txt
+check_token_list() {
+  local LIST="$1"
 
-cat /tmp/orig_erc20_token_list.txt /tmp/modified_erc20_token_list.txt |
-  sort -n | uniq -d >/tmp/union_erc20_token_list.txt
+  echo "Linting $LIST"
 
-diff /tmp/orig_erc20_token_list.txt /tmp/union_erc20_token_list.txt || exit 1
+  git show origin/develop:"$LIST" |
+    jq -r '.tokens[] | .address | ascii_downcase' |
+    sort -n >/tmp/orig_erc20_token_list.txt
 
-# AVAX Tokens
-git show origin/develop:common/tokenlist/avaxtokens/avax_mainnet_V95.json |
-  jq -r '.tokens[] | .address | ascii_downcase' | sort -n | uniq -u >/tmp/orig_avax_erc20_token_list.txt
+  jq -r '.tokens[] | .address | ascii_downcase' <"$LIST" |
+    sort -n >/tmp/modified_erc20_token_list.txt
 
-jq -r '.tokens[] | .address | ascii_downcase' <common/tokenlist/avaxtokens/avax_mainnet_V95.json |
-  uniq -u >/tmp/modified_avax_erc20_token_list.txt
+  # shellcheck disable=SC2155
+  local REMOVALS=$(comm -23 /tmp/orig_erc20_token_list.txt /tmp/modified_erc20_token_list.txt)
+  if [ -n "$REMOVALS" ]; then
+    printf "Tokens removed:\n%s\n" "$REMOVALS"
+    possibly_die
+  fi
 
-cat /tmp/orig_avax_erc20_token_list.txt /tmp/modified_avax_erc20_token_list.txt |
-  sort -n | uniq -d >/tmp/union_avax_erc20_token_list.txt
+  # TODO: enable this check when the existing dupes are removed.
+  # local DUPES=$(cat /tmp/orig_erc20_token_list.txt /tmp/modified_erc20_token_list.txt | sort -n | uniq --count | awk '$1 > 2 {print}')
+  # if [ -n "$DUPES" ]; then
+  #     printf "Tokens already in list:\n%s\n" "$DUPES"
+  #     possibly_die
+  # fi
+}
 
-diff /tmp/orig_avax_erc20_token_list.txt /tmp/union_avax_erc20_token_list.txt || exit 1
+check_token_list common/tokenlist/ethtokens/eth_mainnet_latest.json
+check_token_list common/tokenlist/avaxtokens/avax_mainnet_latest.json
+check_token_list common/tokenlist/bsctokens/bsc_mainnet_latest.json
 
 echo "OK"
