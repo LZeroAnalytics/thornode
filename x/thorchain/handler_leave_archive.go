9c93d46107ac5f655e1963e009f284f5107e86b1
Ursa (9R) <ursa@ninerealms.co>
2024-07-08 15:15:24 +0000

    [cleanup] Leave Event Keys

--- x/thorchain/handler_leave_archive.go
+++ x/thorchain/handler_leave_archive.go
@@ -8,6 +8,85 @@ import (
 	"gitlab.com/thorchain/thornode/constants"
 )
 
+func (h LeaveHandler) handleV124(ctx cosmos.Context, msg MsgLeave) error {
+	nodeAcc, err := h.mgr.Keeper().GetNodeAccount(ctx, msg.NodeAddress)
+	if err != nil {
+		return ErrInternal(err, "fail to get node account by bond address")
+	}
+	if nodeAcc.IsEmpty() {
+		return cosmos.ErrUnknownRequest("node account doesn't exist")
+	}
+	if !nodeAcc.BondAddress.Equals(msg.Tx.FromAddress) {
+		return cosmos.ErrUnauthorized(fmt.Sprintf("%s are not authorized to manage %s", msg.Tx.FromAddress, msg.NodeAddress))
+	}
+	// THORNode add the node to leave queue
+
+	coin := msg.Tx.Coins.GetCoin(common.RuneAsset())
+	if !coin.IsEmpty() {
+		nodeAcc.Bond = nodeAcc.Bond.Add(coin.Amount)
+	}
+	bondAddr, err := nodeAcc.BondAddress.AccAddress()
+	if err != nil {
+		return ErrInternal(err, "fail to refund bond")
+	}
+
+	if nodeAcc.Status == NodeActive {
+		if nodeAcc.LeaveScore == 0 {
+			// get to the 8th decimal point, but keep numbers integers for safer math
+			age := cosmos.NewUint(uint64((ctx.BlockHeight() - nodeAcc.StatusSince) * common.One))
+			slashPts, err := h.mgr.Keeper().GetNodeAccountSlashPoints(ctx, nodeAcc.NodeAddress)
+			if err != nil || slashPts == 0 {
+				ctx.Logger().Error("fail to get node account slash points", "error", err)
+				nodeAcc.LeaveScore = age.Uint64()
+			} else {
+				nodeAcc.LeaveScore = age.QuoUint64(uint64(slashPts)).Uint64()
+			}
+		}
+	} else {
+		bondLockPeriod, err := h.mgr.Keeper().GetMimir(ctx, constants.BondLockupPeriod.String())
+		if err != nil || bondLockPeriod < 0 {
+			bondLockPeriod = h.mgr.GetConstants().GetInt64Value(constants.BondLockupPeriod)
+		}
+		if ctx.BlockHeight()-nodeAcc.StatusSince < bondLockPeriod {
+			return fmt.Errorf("node can not unbond before %d", nodeAcc.StatusSince+bondLockPeriod)
+		}
+		vaults, err := h.mgr.Keeper().GetAsgardVaultsByStatus(ctx, RetiringVault)
+		if err != nil {
+			return ErrInternal(err, "fail to get retiring vault")
+		}
+		isMemberOfRetiringVault := false
+		for _, v := range vaults {
+			if v.GetMembership().Contains(nodeAcc.PubKeySet.Secp256k1) {
+				isMemberOfRetiringVault = true
+				ctx.Logger().Info("node account is still part of the retiring vault,can't return bond yet")
+				break
+			}
+		}
+		if !isMemberOfRetiringVault {
+			// NOTE: there is an edge case, where the first node doesn't have a
+			// vault (it was destroyed when we successfully migrated funds from
+			// their address to a new TSS vault
+			if !h.mgr.Keeper().VaultExists(ctx, nodeAcc.PubKeySet.Secp256k1) {
+				if err := refundBond(ctx, msg.Tx, bondAddr, cosmos.ZeroUint(), &nodeAcc, h.mgr); err != nil {
+					return ErrInternal(err, "fail to refund bond")
+				}
+				nodeAcc.UpdateStatus(NodeDisabled, ctx.BlockHeight())
+			}
+		}
+	}
+	nodeAcc.RequestedToLeave = true
+	if err := h.mgr.Keeper().SetNodeAccount(ctx, nodeAcc); err != nil {
+		return ErrInternal(err, "fail to save node account to key value store")
+	}
+	ctx.EventManager().EmitEvent(
+		cosmos.NewEvent("validator_request_leave",
+			cosmos.NewAttribute("signer bnb address", msg.Tx.FromAddress.String()),
+			cosmos.NewAttribute("destination", nodeAcc.BondAddress.String()),
+			cosmos.NewAttribute("tx", msg.Tx.ID.String())))
+
+	return nil
+}
+
 func (h LeaveHandler) handleV76(ctx cosmos.Context, msg MsgLeave) error {
 	nodeAcc, err := h.mgr.Keeper().GetNodeAccount(ctx, msg.NodeAddress)
 	if err != nil {
