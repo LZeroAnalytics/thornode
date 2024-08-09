95f4f2159121fef0f1dca9f2614fb74ba5c587f1
Pluto (9R) <pluto@ninerealms.co>
2024-08-09 17:59:56 -0400

    enforce halt height

--- app/app_default.go
+++ app/app_default.go
@@ -4,12 +4,20 @@
 package app
 
 import (
+	"os"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
+	"gitlab.com/thorchain/thornode/config"
 )
 
 // BeginBlocker application updates every begin block
 func (app *THORChainApp) BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock) abci.ResponseBeginBlock {
+	haltHeight := config.GetThornode().Cosmos.HaltHeight
+	if haltHeight > 0 && ctx.BlockHeight() > haltHeight {
+		ctx.Logger().Info("halt height reached", "height", ctx.BlockHeight(), "halt height", haltHeight)
+		os.Exit(0)
+	}
 	return app.mm.BeginBlock(ctx, req)
 }
 
