5c17394f5093184b32caffc05ab5ab88607e2753
Ursa <ursa@ninerealms.co>
2024-08-28 17:34:11 -0400

    [fix] Miscellaneous for Stagenet Reset

--- x/thorchain/mimir_address_stagenet.go
+++ x/thorchain/mimir_address_stagenet.go
@@ -5,8 +5,5 @@ package thorchain
 
 // ADMINS hard coded admin address
 var ADMINS = []string{
-	"sthor1lr9hcvvkzclmcdke5kqx4wxeymeng36p7keq0f",
-	"sthor1qwm26kmhctj5ezzrasz3cdccpt8kjjcj5fs0pn",
-	"sthor1ts0txfh3ev7lm9qf839esnartdw62w6lga39t0",
-	"sthor19pkncem64gajdwrd5kasspyj0t75hhkp5t0zkf",
+	"sthor1gdq9qejfy33jctztqdrg5v4hvxnytmvjhduacc",
 }
