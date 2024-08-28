b92e7709ae3593cdd6208eb5e046b72bfc2958c3
Ursa <ursa@ninerealms.co>
2024-08-28 17:34:11 -0400

    [fix] Miscellaneous for Stagenet Reset

--- build/scripts/core.sh
+++ build/scripts/core.sh
@@ -32,12 +32,29 @@ add_node_account() {
   NODE_PUB_KEY_ED25519=$6
   IP_ADDRESS=$7
   MEMBERSHIP=$8
-  jq --arg IP_ADDRESS "$IP_ADDRESS" --arg VERSION "$VERSION" --arg BOND_ADDRESS "$BOND_ADDRESS" --arg VALIDATOR "$VALIDATOR" --arg NODE_ADDRESS "$NODE_ADDRESS" --arg NODE_PUB_KEY "$NODE_PUB_KEY" --arg NODE_PUB_KEY_ED25519 "$NODE_PUB_KEY_ED25519" '.app_state.thorchain.node_accounts += [{"node_address": $NODE_ADDRESS, "version": $VERSION, "ip_address": $IP_ADDRESS, "status": "Active","bond":"100000000", "active_block_height": "0", "bond_address":$BOND_ADDRESS, "signer_membership": [], "validator_cons_pub_key":$VALIDATOR, "pub_key_set":{"secp256k1":$NODE_PUB_KEY,"ed25519":$NODE_PUB_KEY_ED25519}}]' <~/.thornode/config/genesis.json >/tmp/genesis.json
+  jq --arg IP_ADDRESS "$IP_ADDRESS" --arg VERSION "$VERSION" --arg BOND_ADDRESS "$BOND_ADDRESS" --arg VALIDATOR "$VALIDATOR" --arg NODE_ADDRESS "$NODE_ADDRESS" --arg NODE_PUB_KEY "$NODE_PUB_KEY" --arg NODE_PUB_KEY_ED25519 "$NODE_PUB_KEY_ED25519" '.app_state.thorchain.node_accounts += [{"node_address": $NODE_ADDRESS, "version": $VERSION, "ip_address": $IP_ADDRESS, "status": "Active","bond":"30000000000000", "active_block_height": "0", "bond_address":$BOND_ADDRESS, "signer_membership": [], "validator_cons_pub_key":$VALIDATOR, "pub_key_set":{"secp256k1":$NODE_PUB_KEY,"ed25519":$NODE_PUB_KEY_ED25519}}]' <~/.thornode/config/genesis.json >/tmp/genesis.json
   mv /tmp/genesis.json ~/.thornode/config/genesis.json
   if [ -n "$MEMBERSHIP" ]; then
     jq --arg MEMBERSHIP "$MEMBERSHIP" '.app_state.thorchain.node_accounts[-1].signer_membership += [$MEMBERSHIP]' ~/.thornode/config/genesis.json >/tmp/genesis.json
     mv /tmp/genesis.json ~/.thornode/config/genesis.json
   fi
+}
+
+add_account() {
+  jq --arg ADDRESS "$1" --arg ASSET "$2" --arg AMOUNT "$3" '.app_state.auth.accounts += [{
+        "@type": "/cosmos.auth.v1beta1.BaseAccount",
+        "address": $ADDRESS,
+        "pub_key": null,
+        "account_number": "0",
+        "sequence": "0"
+    }]' <~/.thornode/config/genesis.json >/tmp/genesis.json
+  mv /tmp/genesis.json ~/.thornode/config/genesis.json
+
+  jq --arg ADDRESS "$1" --arg ASSET "$2" --arg AMOUNT "$3" '.app_state.bank.balances += [{
+        "address": $ADDRESS,
+        "coins": [ { "denom": $ASSET, "amount": $AMOUNT } ],
+    }]' <~/.thornode/config/genesis.json >/tmp/genesis.json
+  mv /tmp/genesis.json ~/.thornode/config/genesis.json
 }
 
 reserve() {
@@ -60,10 +77,6 @@ init_chain() {
   echo "Init chain"
   thornode init local --chain-id "$CHAIN_ID"
   echo "$SIGNER_PASSWD" | thornode keys list --keyring-backend file
-
-  for user in "$@"; do # iterate over our list of comma separated users "alice,jack"
-    thornode add-genesis-account "$user" 100000000rune
-  done
 }
 
 fetch_node_id() {
@@ -116,4 +129,55 @@ create_thor_user() {
     fi
   fi
   NODE_PUB_KEY_ED25519=$(printf "%s\n%s\n" "$SIGNER_PASSWD" "$SIGNER_SEED_PHRASE" | thornode ed25519)
+}
+
+set_bond_module() {
+  if [ "$NET" = "mocknet" ]; then
+    BOND_MODULE_ADDR="tthor17gw75axcnr8747pkanye45pnrwk7p9c3uhzgff"
+  elif [ "$NET" = "stagenet" ]; then
+    BOND_MODULE_ADDR="sthor17gw75axcnr8747pkanye45pnrwk7p9c3ve0wxj"
+  else
+    echo "Unsupported NET: $NET"
+    exit 1
+  fi
+
+  jq --arg BOND_MODULE_ADDR "$BOND_MODULE_ADDR" \
+    '.app_state.state.accounts += [
+    {
+      "@type": "/cosmos.auth.v1beta1.ModuleAccount",
+      "base_account": {
+        "account_number": "0",
+        "address": $BOND_MODULE_ADDR,
+        "pub_key": null,
+        "sequence": "0"
+      },
+      "name": "bond",
+      "permissions": []
+    }
+  ]' <~/.thornode/config/genesis.json >/tmp/genesis.json
+  mv /tmp/genesis.json ~/.thornode/config/genesis.json
+
+  jq --arg BOND_MODULE_ADDR "$BOND_MODULE_ADDR" \
+    '.app_state.bank.balances += [
+    {
+      "address": $BOND_MODULE_ADDR,
+      "coins": [ { "denom": "rune", "amount": "30000000000000" } ]
+    }
+  ]' <~/.thornode/config/genesis.json >/tmp/genesis.json
+  mv /tmp/genesis.json ~/.thornode/config/genesis.json
+}
+
+set_eth_contract() {
+  jq --arg CONTRACT "$1" '.app_state.thorchain.chain_contracts += [{"chain": "ETH", "router": $CONTRACT}]' ~/.thornode/config/genesis.json >/tmp/genesis.json
+  mv /tmp/genesis.json ~/.thornode/config/genesis.json
+}
+
+set_avax_contract() {
+  jq --arg CONTRACT "$1" '.app_state.thorchain.chain_contracts += [{"chain": "AVAX", "router": $CONTRACT}]' ~/.thornode/config/genesis.json >/tmp/genesis.json
+  mv /tmp/genesis.json ~/.thornode/config/genesis.json
+}
+
+set_bsc_contract() {
+  jq --arg CONTRACT "$1" '.app_state.thorchain.chain_contracts += [{"chain": "BSC", "router": $CONTRACT}]' ~/.thornode/config/genesis.json >/tmp/genesis.json
+  mv /tmp/genesis.json ~/.thornode/config/genesis.json
 }
