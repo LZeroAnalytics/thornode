d4b10054ff68cea9b6c2b8c91bc84db19a4012be
Pluto (9R) <pluto@ninerealms.co>
2025-02-05 17:08:53 -0500

    Separate file for mainnet/non-mainnet migrations

---
+++ x/thorchain/migrations_mainnet.go
@@ -0,0 +1,62 @@
+//go:build mainnet
+// +build mainnet
+
+package thorchain
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"gitlab.com/thorchain/thornode/v3/common"
+	"gitlab.com/thorchain/thornode/v3/common/cosmos"
+	v2 "gitlab.com/thorchain/thornode/v3/x/thorchain/migrations/v2"
+)
+
+// Migrator is a struct for handling in-place store migrations.
+type Migrator struct {
+	mgr *Mgrs
+}
+
+// NewMigrator returns a new Migrator.
+func NewMigrator(mgr *Mgrs) Migrator {
+	return Migrator{mgr: mgr}
+}
+
+// Migrate1to2 migrates from version 1 to 2.
+func (m Migrator) Migrate1to2(ctx sdk.Context) error {
+	// Loads the manager for this migration (we are in the x/upgrade's preblock)
+	// Note, we do not require the manager loaded for this migration, but it is okay
+	// to load it earlier and this is the pattern for migrations to follow.
+	if err := m.mgr.LoadManagerIfNecessary(ctx); err != nil {
+		return err
+	}
+	return v2.MigrateStore(ctx, m.mgr.storeKey)
+}
+
+// Migrate2to3 migrates from version 2 to 3.
+func (m Migrator) Migrate2to3(ctx sdk.Context) error {
+	// Loads the manager for this migration (we are in the x/upgrade's preblock)
+	// Note, we do not require the manager loaded for this migration, but it is okay
+	// to load it earlier and this is the pattern for migrations to follow.
+	if err := m.mgr.LoadManagerIfNecessary(ctx); err != nil {
+		return err
+	}
+
+	// refund stagenet funding wallet for user refund
+	// original user tx: https://runescan.io/tx/A9AF3ED203079BB246CEE0ACD837FBA024BC846784DE488D5BE70044D8877C52
+	// refund to user from stagenet funding wallet: https://bscscan.com/tx/0xba67f3a88f8c998f29e774ffa8328e5625521e37c2db282b29a04ab3d2593f48
+	stagenetWallet := "0x3021C479f7F8C9f1D5c7d8523BA5e22C0Bcb5430"
+	inTxId := "A9AF3ED203079BB246CEE0ACD837FBA024BC846784DE488D5BE70044D8877C52" // original user tx
+
+	bscUsdt, err := common.NewAsset("BSC.USDT-0X55D398326F99059FF775485246999027B3197955")
+	if err != nil {
+		return err
+	}
+	usdtCoin := common.NewCoin(bscUsdt, cosmos.NewUint(4860737515919))
+	blockHeight := ctx.BlockHeight()
+
+	// schedule refund
+	if err := unsafeAddRefundOutbound(ctx, m.mgr, inTxId, stagenetWallet, usdtCoin, blockHeight); err != nil {
+		return err
+	}
+
+	return nil
+}
