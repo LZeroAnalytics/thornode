15e8b2aee33d71058d8d00568f7341920b491841
Multipartite <sonaebito@gmail.com>
2025-03-14 21:16:08 +0200

    ETH/EVM DustThreshold (/thorchain/inbound_addresses dust_threshold) to return 1 rather than the current 0 (which is not observed)

--- bifrost/pkg/chainclients/ethereum/ethereum_block_scanner.go
+++ bifrost/pkg/chainclients/ethereum/ethereum_block_scanner.go
@@ -856,7 +856,7 @@ func (e *ETHScanner) getTxInFromTransaction(tx *etypes.Transaction, receipt *ety
 			// When the Sender and To is the same then there's no balance chance whatever the Coins,
 			// and for Tx-received Valid() a non-zero Amount is needed to observe
 			// the transaction fees (THORChain gas cost) of unstuck.go's cancel transactions.
-			observableAmount := common.ETHChain.DustThreshold().Add(cosmos.OneUint()) // Adding 1 for if DustThreshold is 0.
+			observableAmount := common.ETHChain.DustThreshold()
 			txInItem.Coins = common.NewCoins(common.NewCoin(common.ETHAsset, observableAmount))
 
 			// remove the outbound from signer cache so it can be re-attempted
