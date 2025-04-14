f57472ac0e58313f88bc214471f2e46ae6f7da5f
BitolMaya <bitol@mayaprotocol.com>
2025-04-14 13:53:27 -0600

    Update TCY contract addresses

--- common/tcysmartcontract/tcy_smart_contract_mocknet.go
+++ common/tcysmartcontract/tcy_smart_contract_mocknet.go
@@ -4,6 +4,6 @@
 package tcysmartcontract
 
 const (
-	// TODO: Update with real smart contract address
+	// Update with local smart contract address for testing
 	TCYSmartContractAddress = "tthor1hjpct8pd9d48vyqltaqunltwx9twm57l4h2dac"
 )
