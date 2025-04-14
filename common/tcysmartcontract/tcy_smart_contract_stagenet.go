e1d15b827e4773db6fbcdce66b43ca629b97ce91
BitolMaya <bitol@mayaprotocol.com>
2025-04-14 13:53:27 -0600

    Update TCY contract addresses

--- common/tcysmartcontract/tcy_smart_contract_stagenet.go
+++ common/tcysmartcontract/tcy_smart_contract_stagenet.go
@@ -4,6 +4,6 @@
 package tcysmartcontract
 
 const (
-	// TODO: Update with real smart contract address
-	TCYSmartContractAddress = "sthor1hjpct8pd9d48vyqltaqunltwx9twm57l9e8tjr"
+	// From 3e33eee1b1fb4f58fe23e381808a32486c462680515a94fb1103099df6501ad8 code checksum in wasm permissions
+	TCYSmartContractAddress = "sthor1zyndq53kghzv9mujyv9wkxfhmlpq3r0dznrwq4llzjz74ue4wykq8zz0pq"
 )
