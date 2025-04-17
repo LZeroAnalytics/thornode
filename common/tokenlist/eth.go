91e2c51d25d4edf0a6045538658fd0cacdd2a6cf
Oleg Petrov <oleg.petrov.eth@gmail.com>
2025-04-17 12:57:41 +0000

    DEX Agg updates

--- common/tokenlist/eth.go
+++ common/tokenlist/eth.go
@@ -3,23 +3,17 @@ package tokenlist
 import (
 	"encoding/json"
 
-	"github.com/blang/semver"
 	"gitlab.com/thorchain/thornode/v3/common/tokenlist/ethtokens"
 )
 
-var ethTokenListV3_0_0 EVMTokenList
+var ethTokenList EVMTokenList
 
 func init() {
-	if err := json.Unmarshal(ethtokens.ETHTokenListRawV3_0_0, &ethTokenListV3_0_0); err != nil {
+	if err := json.Unmarshal(ethtokens.ETHTokenListRaw, &ethTokenList); err != nil {
 		panic(err)
 	}
 }
 
-func GetETHTokenList(version semver.Version) EVMTokenList {
-	switch {
-	case version.GTE(semver.MustParse("3.0.0")):
-		return ethTokenListV3_0_0
-	default:
-		return EVMTokenList{}
-	}
+func GetETHTokenList() EVMTokenList {
+	return ethTokenList
 }
