cc091689b45f13d60629e1135a05713e5296c293
Bitol Maya <bitol@mayaprotocol.com>
2025-04-22 18:00:14 +0000

    Update TCY Contract Addresses and Change Pool Seed Address to Treasury

--- common/tcysmartcontract/tcy_smart_contract_stagenet.go
+++ common/tcysmartcontract/tcy_smart_contract_stagenet.go
@@ -5,5 +5,5 @@ package tcysmartcontract
 
 const (
 	// From 3e33eee1b1fb4f58fe23e381808a32486c462680515a94fb1103099df6501ad8 code checksum in wasm permissions
-	TCYSmartContractAddress = "sthor1zyndq53kghzv9mujyv9wkxfhmlpq3r0dznrwq4llzjz74ue4wykq8zz0pq"
+	TCYSmartContractAddress = "sthor1z7ejlk5wk2pxh9nfwjzkkdnrq4p2f5rjcpudltv0gh282dwfz6nquvxt53"
 )
