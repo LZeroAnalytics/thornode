f9a696d17b5660ad24cef92dd93dede42421fcfa
Hans Code <codehans@kujira.app>
2025-04-28 19:57:21 +0000

    Update rujira-mint contract permissions

--- common/wasmpermissions/wasm_permissions_mainnet.go
+++ common/wasmpermissions/wasm_permissions_mainnet.go
@@ -5,6 +5,13 @@ package wasmpermissions
 
 var WasmPermissionsRaw = WasmPermissions{
 	Permissions: map[string]WasmPermission{
+		// rujira-mint v1.0.1
+		"86dbc41f7c31bde07e426351cb96c2f73d9584a34e46913119225f178d19e8de": {
+			Origin: "https://gitlab.com/thorchain/rujira/-/tree/25252ec557320d3fb507ad906e08ffa4fa4f5494/contracts/rujira-mint",
+			Deployers: map[string]bool{
+				"thor1e0lmk5juawc46jwjwd0xfz587njej7ay5fh6cd": true,
+			},
+		},
 		// rujira-fin (trade) v1.0.0
 		"de9cac49b68ae4dfe5d6d896e0a4f6f4954e809b50dc6733a0c49b2bb0d25bd9": {
 			Origin: "https://gitlab.com/thorchain/rujira/-/tree/80b48eddc0f16f735855442fdbc5423ac5398ff6/contracts/rujira-fin",
@@ -50,14 +57,6 @@ var WasmPermissionsRaw = WasmPermissions{
 		// rujira-merge v1.0.0
 		"dab37041278fe3b13e7a401918b09e8fd232aaec7b00b5826cf9ecd9d34991ba": {
 			Origin: "https://gitlab.com/thorchain/rujira/-/tree/0ff0376fd8316ad6cb4e4c306a215c7cbb3e29f6/contracts/rujira-merge",
-			Deployers: map[string]bool{
-				"thor1e0lmk5juawc46jwjwd0xfz587njej7ay5fh6cd": true,
-			},
-		},
-
-		// rujira-mint v1.0.0
-		"eb361f43e7e2c00347f03903ba07d567fc9f47b1399dc078060bbcaefc6aafe2": {
-			Origin: "https://gitlab.com/thorchain/rujira/-/tree/52716f6b83af191d7c2cc261b15c6f08cf9b9836/contracts/rujira-mint",
 			Deployers: map[string]bool{
 				"thor1e0lmk5juawc46jwjwd0xfz587njej7ay5fh6cd": true,
 			},
