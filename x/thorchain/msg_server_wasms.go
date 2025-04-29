78f6f0901683890c98410b128a4c75032b78efa4
Hans Code <codehans@kujira.app>
2025-04-29 12:28:22 +0000

    Return wasm msg results

--- x/thorchain/msg_server_wasms.go
+++ x/thorchain/msg_server_wasms.go
@@ -4,40 +4,35 @@ import (
 	"context"
 
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (ms msgServer) StoreCode(goCtx context.Context, msg *wasmtypes.MsgStoreCode) (*wasmtypes.MsgStoreCodeResponse, error) {
-	handler := NewWasmStoreCodeHandler(ms.mgr)
-	_, err := externalHandler(goCtx, handler, msg)
-	return &wasmtypes.MsgStoreCodeResponse{}, err
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	return NewWasmStoreCodeHandler(ms.mgr).Run(ctx, msg)
 }
 
 func (ms msgServer) InstantiateContract(goCtx context.Context, msg *wasmtypes.MsgInstantiateContract) (*wasmtypes.MsgInstantiateContractResponse, error) {
-	handler := NewWasmInstantiateContractHandler(ms.mgr)
-	_, err := externalHandler(goCtx, handler, msg)
-	return &wasmtypes.MsgInstantiateContractResponse{}, err
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	return NewWasmInstantiateContractHandler(ms.mgr).Run(ctx, msg)
 }
 
 func (ms msgServer) InstantiateContract2(goCtx context.Context, msg *wasmtypes.MsgInstantiateContract2) (*wasmtypes.MsgInstantiateContract2Response, error) {
-	handler := NewWasmInstantiateContract2Handler(ms.mgr)
-	_, err := externalHandler(goCtx, handler, msg)
-	return &wasmtypes.MsgInstantiateContract2Response{}, err
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	return NewWasmInstantiateContract2Handler(ms.mgr).Run(ctx, msg)
 }
 
 func (ms msgServer) ExecuteContract(goCtx context.Context, msg *wasmtypes.MsgExecuteContract) (*wasmtypes.MsgExecuteContractResponse, error) {
-	handler := NewWasmExecuteContractHandler(ms.mgr)
-	_, err := externalHandler(goCtx, handler, msg)
-	return &wasmtypes.MsgExecuteContractResponse{}, err
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	return NewWasmExecuteContractHandler(ms.mgr).Run(ctx, msg)
 }
 
 func (ms msgServer) MigrateContract(goCtx context.Context, msg *wasmtypes.MsgMigrateContract) (*wasmtypes.MsgMigrateContractResponse, error) {
-	handler := NewWasmMigrateContractHandler(ms.mgr)
-	_, err := externalHandler(goCtx, handler, msg)
-	return &wasmtypes.MsgMigrateContractResponse{}, err
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	return NewWasmMigrateContractHandler(ms.mgr).Run(ctx, msg)
 }
 
 func (ms msgServer) SudoContract(goCtx context.Context, msg *wasmtypes.MsgSudoContract) (*wasmtypes.MsgSudoContractResponse, error) {
-	handler := NewWasmSudoContractHandler(ms.mgr)
-	_, err := externalHandler(goCtx, handler, msg)
-	return &wasmtypes.MsgSudoContractResponse{}, err
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	return NewWasmSudoContractHandler(ms.mgr).Run(ctx, msg)
 }
