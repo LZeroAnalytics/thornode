abc1e970c6ad0bac957c1f515cf8a6fd47b8f1ba
Andrew Gouin <andrew@gouin.io>
2025-05-06 01:54:31 +0000

    [fix] ensure proposal cannot go above max tx size

--- x/thorchain/keeper/abci/abci.go
+++ x/thorchain/keeper/abci/abci.go
@@ -2,6 +2,7 @@ package abci
 
 import (
 	abci "github.com/cometbft/cometbft/abci/types"
+	cmttypes "github.com/cometbft/cometbft/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	ebifrost "gitlab.com/thorchain/thornode/v3/x/thorchain/ebifrost"
 	"gitlab.com/thorchain/thornode/v3/x/thorchain/keeper"
@@ -30,13 +31,32 @@ func NewProposalHandler(
 }
 
 func (h *ProposalHandler) PrepareProposal(ctx sdk.Context, req *abci.RequestPrepareProposal) (*abci.ResponsePrepareProposal, error) {
-	injectTxs := h.bifrost.ProposalInjectTxs(ctx)
+	injectTxs, txBzLen := h.bifrost.ProposalInjectTxs(ctx, req.MaxTxBytes)
 
-	if len(injectTxs) > 0 {
-		req.Txs = append(injectTxs, req.Txs...)
+	// Modify request for upstream handler with reduced max tx size
+	req.MaxTxBytes -= txBzLen
+
+	// TODO remove this after upgrading to cosmos sdk that has this fix
+	// https://github.com/cosmos/cosmos-sdk/pull/24074
+	// Note: not critical to do ASAP but will open up some space for more txs
+	var toRemove int64
+	for _, txBz := range req.Txs {
+		amount := int64(len(txBz))
+		protoAmount := cmttypes.ComputeProtoSizeForTxs([]cmttypes.Tx{txBz})
+		toRemove += protoAmount - amount
+	}
+	req.MaxTxBytes -= toRemove
+	// END TODO
+
+	// Let default handler process original txs with reduced size
+	resp, err := h.prepareProposalHandler(ctx, req)
+	if err != nil {
+		return nil, err
 	}
 
-	return h.prepareProposalHandler(ctx, req)
+	// Combine ebifrost inject txs with the ones selected by default handler
+	combinedTxs := append(injectTxs, resp.Txs...) //nolint:gocritic
+	return &abci.ResponsePrepareProposal{Txs: combinedTxs}, nil
 }
 
 func (h *ProposalHandler) ProcessProposal(ctx sdk.Context, req *abci.RequestProcessProposal) (*abci.ResponseProcessProposal, error) {
