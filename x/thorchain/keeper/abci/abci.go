ccc7d9b3055850d8143522fb3915aa1f168e571a
Andrew Gouin <andrew@gouin.io>
2025-05-06 15:36:13 +0000

    [fix] proposal mempool tx size overrun

--- x/thorchain/keeper/abci/abci.go
+++ x/thorchain/keeper/abci/abci.go
@@ -34,6 +34,7 @@ func (h *ProposalHandler) PrepareProposal(ctx sdk.Context, req *abci.RequestPrep
 	injectTxs, txBzLen := h.bifrost.ProposalInjectTxs(ctx, req.MaxTxBytes)
 
 	// Modify request for upstream handler with reduced max tx size
+	origMaxTxBytes := req.MaxTxBytes
 	req.MaxTxBytes -= txBzLen
 
 	// TODO remove this after upgrading to cosmos sdk that has this fix
@@ -54,8 +55,39 @@ func (h *ProposalHandler) PrepareProposal(ctx sdk.Context, req *abci.RequestPrep
 		return nil, err
 	}
 
-	// Combine ebifrost inject txs with the ones selected by default handler
-	combinedTxs := append(injectTxs, resp.Txs...) //nolint:gocritic
+	// Combine ebifrost inject txs with the ones selected by default handler, up to tx limit
+	combinedTxs := injectTxs
+
+	defaultTxsSize := int64(0)
+	totalSize := txBzLen
+	for _, tx := range resp.Txs {
+		txSize := cmttypes.ComputeProtoSizeForTxs([]cmttypes.Tx{tx})
+		defaultTxsSize += txSize
+		if totalSize+txSize <= origMaxTxBytes {
+			totalSize += txSize
+			combinedTxs = append(combinedTxs, tx)
+		} else {
+			ctx.Logger().Warn(
+				"Dropping transaction that would exceed block size limit",
+				"current_size", totalSize,
+				"tx_size", txSize,
+				"max_size", origMaxTxBytes,
+			)
+		}
+	}
+
+	ctx.Logger().Info(
+		"Proposal Transaction sizes",
+		"injected_txs_count", len(injectTxs),
+		"injected_txs_size", txBzLen,
+		"default_txs_count", len(resp.Txs),
+		"default_txs_size", defaultTxsSize,
+		"final_txs_size", totalSize,
+		"final_txs_count", len(combinedTxs),
+		"max_bytes", req.MaxTxBytes,
+		"original_max_bytes", origMaxTxBytes,
+	)
+
 	return &abci.ResponsePrepareProposal{Txs: combinedTxs}, nil
 }
 
