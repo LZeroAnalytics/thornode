913d58c61f9466cca34476e99523b16ebe555276
Andrew Gouin <andrew@gouin.io>
2025-05-06 12:48:41 +0000

    2 second blocks

--- x/thorchain/genesis.go
+++ x/thorchain/genesis.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"sort"
 	"strings"
+	"time"
 
 	abci "github.com/cometbft/cometbft/abci/types"
 	"gitlab.com/thorchain/thornode/v3/common"
@@ -488,7 +489,7 @@ func ExportGenesis(ctx cosmos.Context, k keeper.Keeper) GenesisState {
 	if startBlockHeight < 1 {
 		startBlockHeight = 1
 	}
-	endBlockHeight := ctx.BlockHeight() + 17200
+	endBlockHeight := ctx.BlockHeight() + constants.BlocksIn(30*time.Minute)
 
 	for height := startBlockHeight; height < endBlockHeight; height++ {
 		txOut, err := k.GetTxOut(ctx, height)
