1e3db570e6dd2d033942b5a9241a0d44651e5e3a
Zefiro <gitlab.pushy576@passmail.net>
2025-06-26 13:26:12 +0000

    Patch WASM Permission: Fix wrong redemption rate calculation in Nami Index Nav (v3.8 cutoff)

--- common/wasmpermissions/groups/nami.go
+++ common/wasmpermissions/groups/nami.go
@@ -3,9 +3,9 @@ package groups
 import "gitlab.com/thorchain/thornode/v3/common/wasmpermissions/types"
 
 var NamiPermissions = map[string]types.WasmPermission{
-	// nami-index-nav v1.0.0
-	"d20dc480a8484242f72c7f1e8db0bc39e5da48f93a4cc4fa679d9e8acff65a62": {
-		Origin: "https://github.com/NAMIProtocol/nami-contracts/tree/3efb8706f2438323d5dbae29c337a11a6509de30/contracts/nami-index-nav",
+	// nami-index-nav v1.0.1
+	"e452f0568a1d73f4fb1a61f37df4c19ddd3cf48938fca39f3fb23022d4ddc8dc": {
+		Origin: "https://github.com/NAMIProtocol/nami-contracts/tree/0e1f2ea4ca9d8a7130d13b0de5c0d4914154c1a0/contracts/nami-index-nav",
 		Deployers: map[string]bool{
 			"thor1zjwanvezcjp6hefgt6vqfnrrdm8yj9za3s8ss0": true,
 		},
