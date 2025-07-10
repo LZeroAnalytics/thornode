8e93b938df5e7fa974284a6ed771d4877da5317b
Hans <hans@rujira.network>
2025-07-10 17:43:13 +0000

    [App Layer] Make wasm permissions a list of deployers

--- common/wasmpermissions/wasm_permissions_mocknet.go
+++ common/wasmpermissions/wasm_permissions_mocknet.go
@@ -3,17 +3,15 @@
 
 package wasmpermissions
 
-import "gitlab.com/thorchain/thornode/v3/common/wasmpermissions/types"
-
 var WasmPermissionsRaw = WasmPermissions{
-	Permissions: map[string]types.WasmPermission{
-		"a8f1a38aa518864169e30ab482ea86558a817982a030b8888ea6dfa0cd700128": {
-			Origin: "https://thorchain.org",
-			Deployers: map[string]bool{
-				"tthor1jgnk2mg88m57csrmrlrd6c3qe4lag3e33y2f3k": true,
-				"tthor1tdfqy34uptx207scymqsy4k5uzfmry5sf7z3dw": false,
-				"tthor1khtl8ch2zgay00c47ukvulam3a4faw2500g7lu": true,
-			},
-		},
+	Store: map[string]bool{
+		"tthor1jgnk2mg88m57csrmrlrd6c3qe4lag3e33y2f3k": true,
+		"tthor1tdfqy34uptx207scymqsy4k5uzfmry5sf7z3dw": false,
+		"tthor1khtl8ch2zgay00c47ukvulam3a4faw2500g7lu": true,
+	},
+	Instantiate: map[string]bool{
+		"tthor1jgnk2mg88m57csrmrlrd6c3qe4lag3e33y2f3k": true,
+		"tthor1tdfqy34uptx207scymqsy4k5uzfmry5sf7z3dw": false,
+		"tthor1khtl8ch2zgay00c47ukvulam3a4faw2500g7lu": true,
 	},
 }
