ac8a504cd2d2398c816dd0aacd08911b66c99e08
Til Jordan <til@bloctopus.io>
2025-07-23 20:46:57 +0000

    feat: extend CtxUserAPICall flag to all APIs via universal metadata
    - Set user-api-call metadata in CustomGRPCGatewayRouter for all requests
    - Update thorchain and denom query handlers to extract metadata
    - Ensures shouldAllowRemoteFetch() evaluates to true for all user requests

--- x/denom/keeper/grpc_query.go
+++ x/denom/keeper/grpc_query.go
@@ -3,8 +3,10 @@ package keeper
 import (
 	"context"
 
+	"google.golang.org/grpc/metadata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	"gitlab.com/thorchain/thornode/v3/constants"
 	"gitlab.com/thorchain/thornode/v3/x/denom/types"
 )
 
@@ -12,6 +14,12 @@ var _ types.QueryServer = Keeper{}
 
 func (k Keeper) DenomAdmin(ctx context.Context, req *types.QueryDenomAdminRequest) (*types.QueryDenomAdminResponse, error) {
 	sdkCtx := sdk.UnwrapSDKContext(ctx)
+	
+	if md, ok := metadata.FromIncomingContext(ctx); ok {
+		if userFlag := md.Get("user-api-call"); len(userFlag) > 0 && userFlag[0] == "true" {
+			sdkCtx = sdkCtx.WithContext(context.WithValue(sdkCtx.Context(), constants.CtxUserAPICall, true))
+		}
+	}
 
 	admin, err := k.GetAdmin(sdkCtx, req.GetDenom())
 	if err != nil {
