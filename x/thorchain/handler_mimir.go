73a9f49c02d8303494a02d7a34a82cc0cb149211
Ursa (9R) <ursa@ninerealms.co>
2025-08-04 18:33:02 +0000

    [feature] Bounded Operational Min Slip Mimirs

--- x/thorchain/handler_mimir.go
+++ x/thorchain/handler_mimir.go
@@ -47,16 +47,6 @@ func (h MimirHandler) Run(ctx cosmos.Context, m cosmos.Msg) (*cosmos.Result, err
 }
 
 func (h MimirHandler) validate(ctx cosmos.Context, msg MsgMimir) error {
-	version := h.mgr.GetVersion()
-	switch {
-	case version.GTE(semver.MustParse("3.0.0")):
-		return h.validateV3_0_0(ctx, msg)
-	default:
-		return errBadVersion
-	}
-}
-
-func (h MimirHandler) validateV3_0_0(ctx cosmos.Context, msg MsgMimir) error {
 	// ValidateBasic is also executed in message service router's handler and isn't versioned there
 	if err := msg.ValidateBasic(); err != nil {
 		return err
@@ -68,22 +58,23 @@ func (h MimirHandler) validateV3_0_0(ctx cosmos.Context, msg MsgMimir) error {
 	if _, err := validateMimirAuth(ctx, h.mgr.Keeper(), msg); err != nil {
 		return err
 	}
+
+	// nothing else to verify if this is not a min slip mimir
+	if !strings.HasSuffix(strings.ToUpper(msg.Key), "SLIPMINBPS") {
+		return nil
+	}
+
+	// operational min slip validation
+	minSlipBpsMax := h.mgr.Keeper().GetConfigInt64(ctx, constants.SlipMinBpsMax)
+	if msg.Value > minSlipBpsMax {
+		return cosmos.ErrUnknownRequest("min slip mimir exceeds SlipMinBpsMax")
+	}
 	return nil
 }
 
 func (h MimirHandler) handle(ctx cosmos.Context, msg MsgMimir) error {
 	ctx.Logger().Info("handleMsgMimir request", "node", msg.Signer, "key", msg.Key, "value", msg.Value)
-	version := h.mgr.GetVersion()
-	switch {
-	case version.GTE(semver.MustParse("3.0.0")):
-		return h.handleV3_0_0(ctx, msg)
-	default:
-		ctx.Logger().Error(errInvalidVersion.Error())
-		return errBadVersion
-	}
-}
 
-func (h MimirHandler) handleV3_0_0(ctx cosmos.Context, msg MsgMimir) error {
 	// Get the current Mimir key value if it exists.
 	currentMimirValue, _ := h.mgr.Keeper().GetMimir(ctx, msg.Key)
 	// Here, an error is assumed to mean the Mimir key is currently unset.
