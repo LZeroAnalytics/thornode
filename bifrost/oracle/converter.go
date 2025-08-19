485425d331878f6722840ee395611f685f0ffd5e
Starsquid <22675214-starsquid@users.noreply.gitlab.com>
2025-08-19 11:16:05 +0200

    Catch empty filtered rates

--- bifrost/oracle/converter.go
+++ bifrost/oracle/converter.go
@@ -73,6 +73,10 @@ func (c *Converter) GetRate(symbol string, metrics bool) (*big.Float, *big.Float
 		c.metrics.UpdateProviderBounds(symbol, upperBound, lowerBound)
 	}
 
+	if len(rates) == 0 {
+		return nil, nil, fmt.Errorf("no rates found")
+	}
+
 	volume := new(big.Float)
 
 	prices := map[string]*big.Float{}
