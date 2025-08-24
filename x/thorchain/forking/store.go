e8abafb35fb526e5d05a55804bfd223d3e680f7f
Til Jordan <til@bloctopus.io>
2025-08-24 16:16:24 +0000

    forking/store: always log wasm 0x02 ContractInfo key at Get start for decode debugging

--- x/thorchain/forking/store.go
+++ x/thorchain/forking/store.go
@@ -80,6 +80,9 @@ func (f *forkingKVStore) shouldAllowRemoteFetch() bool {
 }
 
 func (f *forkingKVStore) Get(key []byte) ([]byte, error) {
+	if f.storeKey == "wasm" && len(key) > 0 && key[0] == 0x02 {
+		fmt.Printf("[forking][GET][wasm] ContractInfo key len=%d key=%s\n", len(key), hex.EncodeToString(key))
+	}
 	if v, err := f.parent.Get(key); err == nil && v != nil {
 		return v, nil
 	}
@@ -91,9 +94,6 @@ func (f *forkingKVStore) Get(key []byte) ([]byte, error) {
 				} else {
 					fmt.Printf("[forking][GET] negative-cache-hit store=%s key=%s\n", f.storeKey, hex.EncodeToString(key))
 					f.service.updateStats(false, true, 0, false)
-	if f.storeKey == "wasm" && len(key) > 0 && key[0] == 0x02 {
-		fmt.Printf("[forking][GET][wasm] ContractInfo key len=%d key=%s\n", len(key), hex.EncodeToString(key))
-	}
 					return nil, nil
 				}
 			} else {
