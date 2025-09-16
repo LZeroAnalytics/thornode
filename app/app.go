9d13e950132f11a2206271ed9808c99e7984aaa6
Til Jordan <til@bloctopus.io>
2025-09-16 16:45:23 +0000

    app(query): route Tx/Tendermint services through custom GRPCQueryRouter; add debug logs for bank/auth/acc RPC path

--- app/app.go
+++ app/app.go
@@ -93,6 +93,7 @@ import (
 	denomtypes "gitlab.com/thorchain/thornode/v3/x/denom/types"
 
 	evm "github.com/cosmos/evm/encoding/codec"
+
 	"github.com/cosmos/evm/ethereum/eip712"
 )
 
@@ -1013,7 +1014,7 @@ func RegisterSwaggerAPI(rtr *mux.Router, swaggerEnabled bool) error {
 
 // RegisterTxService implements the Application.RegisterTxService method.
 func (app *THORChainApp) RegisterTxService(clientCtx client.Context) {
-	authtx.RegisterTxService(app.BaseApp.GRPCQueryRouter(), clientCtx, app.BaseApp.Simulate, app.interfaceRegistry)
+	authtx.RegisterTxService(app.GRPCQueryRouter(), clientCtx, app.BaseApp.Simulate, app.interfaceRegistry)
 }
 
 // RegisterTendermintService implements the Application.RegisterTendermintService method.
@@ -1021,7 +1022,7 @@ func (app *THORChainApp) RegisterTendermintService(clientCtx client.Context) {
 	cmtApp := server.NewCometABCIWrapper(app)
 	cmtservice.RegisterTendermintService(
 		clientCtx,
-		app.BaseApp.GRPCQueryRouter(),
+		app.GRPCQueryRouter(),
 		app.interfaceRegistry,
 		cmtApp.Query,
 	)
@@ -1065,6 +1066,10 @@ func BlockedAddresses() map[string]bool {
 	delete(modAccAddrs, authtypes.NewModuleAddress(thorchaintypes.TreasuryName).String())
 
 	return modAccAddrs
+}
+
+func (app *THORChainApp) GRPCQueryRouter() *QueryServiceRouter {
+	return app.queryServiceRouter
 }
 
 // MsgServiceRouter returns the MsgServiceRouter.
--- x/thorchain/forking/store.go
+++ x/thorchain/forking/store.go
@@ -1,12 +1,13 @@
 package forking
 
-	"os"
 import (
 	"context"
 	"encoding/hex"
 	"fmt"
-	"gitlab.com/thorchain/thornode/v3/constants"
+	"os"
 	"time"
+
+	"gitlab.com/thorchain/thornode/v3/constants"
 
 	storetypes "cosmossdk.io/core/store"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -90,10 +91,10 @@ func (f *forkingKVStore) shouldAllowRemoteFetch() bool {
 	return true
 }
 
+func (f *forkingKVStore) Get(key []byte) ([]byte, error) {
 	if os.Getenv("THOR_FORK_DEBUG") == "1" && (f.storeKey == "bank" || f.storeKey == "auth" || f.storeKey == "acc") {
 		fmt.Printf("[rpc-debug][GET] store=%s key=%s allowRemote=%v\n", f.storeKey, hex.EncodeToString(key), f.shouldAllowRemoteFetch())
 	}
-func (f *forkingKVStore) Get(key []byte) ([]byte, error) {
 	if f.storeKey == "wasm" && len(key) > 0 && key[0] == 0x02 {
 		fmt.Printf("[forking][GET][wasm] ContractInfo key len=%d key=%s\n", len(key), hex.EncodeToString(key))
 	}
@@ -193,11 +194,11 @@ func (f *forkingKVStore) Delete(key []byte) error {
 	return nil
 }
 
+func (f *forkingKVStore) Iterator(start, end []byte) (storetypes.Iterator, error) {
 	if os.Getenv("THOR_FORK_DEBUG") == "1" && (f.storeKey == "bank" || f.storeKey == "auth" || f.storeKey == "acc") {
 		s, e := start, end
 		fmt.Printf("[rpc-debug][ITER] store=%s start=%s end=%s allowRemote=%v\n", f.storeKey, hex.EncodeToString(s), hex.EncodeToString(e), f.shouldAllowRemoteFetch())
 	}
-func (f *forkingKVStore) Iterator(start, end []byte) (storetypes.Iterator, error) {
 	localIter, err := f.parent.Iterator(start, end)
 	if err != nil {
 		fmt.Printf("[forking][ITER] local-err store=%s err=%v\n", f.storeKey, err)
@@ -215,11 +216,11 @@ func (f *forkingKVStore) Iterator(start, end []byte) (storetypes.Iterator, error
 	return NewMergedIterator(localIter, remoteIter), nil
 }
 
+func (f *forkingKVStore) ReverseIterator(start, end []byte) (storetypes.Iterator, error) {
 	if os.Getenv("THOR_FORK_DEBUG") == "1" && (f.storeKey == "bank" || f.storeKey == "auth" || f.storeKey == "acc") {
 		s, e := start, end
 		fmt.Printf("[rpc-debug][RITER] store=%s start=%s end=%s allowRemote=%v\n", f.storeKey, hex.EncodeToString(s), hex.EncodeToString(e), f.shouldAllowRemoteFetch())
 	}
-func (f *forkingKVStore) ReverseIterator(start, end []byte) (storetypes.Iterator, error) {
 	localIter, err := f.parent.ReverseIterator(start, end)
 	if err != nil {
 		fmt.Printf("[forking][RITER] local-err store=%s err=%v\n", f.storeKey, err)
